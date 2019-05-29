87a82eef4bb4637809814d98064b2fcd71100f13
riggi <simone.riggi@gmail.com>
2019-05-29 12:03:35 +0200

    Add init file

---
+++ sfindernn/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+__title__ = 'sfindernn'
+__version__ = '1.0.0'
+__author__ = 'Simone Riggi'
+__license__ = 'GPL3'
+__date__ = '2019-05-29'
+__copyright__ = 'Copyright 2019 by Simone Riggi - INAF'
+__citation__ = """
