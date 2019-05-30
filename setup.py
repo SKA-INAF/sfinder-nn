57bda4fd7f9e1bdc6ab6ab314c7b14bbbf0b5953
riggi <simone.riggi@gmail.com>
2019-05-30 17:48:21 +0200

    Add future to req modules

--- setup.py
+++ setup.py
@@ -19,7 +19,13 @@ def get_version():
 
 
 reqs = ['numpy>=1.10',
-        'astropy>=2.0, <3']
+        'astropy>=2.0, <3',
+        'matplotlib',
+        'keras>=2.0',
+        'tensorflow>=1.13',
+        'scikit-learn',
+        'future']
+
 
 #if sys.version_info < (2, 7):
 #	reqs.append('lmfit==0.9.1')
@@ -35,12 +41,11 @@ setup(
 	author_email="simone.riggi@gmail.com",
 	description="Source finder based on Neural Networks",
 	license = "GPL3",
-	# keywords="example documentation tutorial",
 	url="https://github.com/SKA-INAF/sfinder-nn",
 	long_description=read('README.md'),
 	packages=['sfindernn'],
 	install_requires=reqs,
-	scripts=['scripts/generate_data.py'],
+	scripts=['scripts/generate_data.py','scripts/train_nn.py','test/test_train.sh','test/test_gendata.sh'],
 	#data_files=[('sfindernn', [os.path.join(data_dir, 'MOC.fits')]) ],
 	#setup_requires=['pytest-runner'],
 	#tests_require=['pytest', 'nose']
